07a801daf38c52ff042b1da4153661283573b81e
Philipp Franck <philipp.franck@dainst.de>
2017-01-26 11:15:49 +0100

    typo

--- classes/frontpageCreator.class.php
+++ classes/frontpageCreator.class.php
@@ -303,7 +303,7 @@ class frontpageCreator {
 		$articleFileManager = new ArticleFileManager($article->getId());
 		$fileId = $articleFileManager->copyPublicFile($newFile, 'application/pdf');
 		if ($fileId == 0) {
-			thorw new Exception("article " . $article->getId() . ": new file could not be copied!");
+			throw new Exception("article " . $article->getId() . ": new file could not be copied!");
 		}
 		$newGalley->setFileId($fileId);	
 		$newGalley->setSequence(0);
