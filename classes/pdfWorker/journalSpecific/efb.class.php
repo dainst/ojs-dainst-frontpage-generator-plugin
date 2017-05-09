2508a435d82c86197b64785c1c61c7f92b2004dd
Philipp Franck <philipp.franck@dainst.de>
2017-05-09 15:36:20 +0200

    correct classnames in journal specific files

--- classes/pdfWorker/journalSpecific/efb.class.php
+++ classes/pdfWorker/journalSpecific/efb.class.php
@@ -1,6 +1,6 @@
 <?php
 namespace dfm\pdfWorkers {
-	class aa extends \dfm\pdfWorker {
+	class efb extends \dfm\pdfWorker {
 		function setMetadata($data) {
 			$this->metadata['terms'] 	= "Die e-Forschungsberichte {$this->metadata['year']}-{$this->metadata['volume']} des DAI stehen unter der Creative-Commons-Lizenz Namensnennung – Nicht kommerziell – Keine Bearbeitungen 4.0 International. 
 			Um eine Kopie dieser Lizenz zu sehen, besuchen Sie bitte http://creativecommons.org/licenses/by-nc-nd/4.0/";
