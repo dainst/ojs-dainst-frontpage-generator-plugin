500261a8a118abffc4f3dd8d4a2e18205c5e1d9e
Lukas C. Bossert <lukas@digitales-altertum.de>
2017-05-09 16:17:23 +0200

    english terms added

--- classes/pdfWorker/journalSpecific/efb.class.php
+++ classes/pdfWorker/journalSpecific/efb.class.php
@@ -2,9 +2,10 @@
 namespace dfm\pdfWorkers {
 	class efb extends \dfm\pdfWorker {
 		function setMetadata($data) {
-			$this->lang->terms->de 	= "<b style=\"font-family:calibrib\">Nutzungsbedingungen:</b> Die e-Forschungsberichte {$this->metadata['year']}-{$this->metadata['volume']} des DAI stehen unter der Creative-Commons-Lizenz Namensnennung – Nicht kommerziell – Keine Bearbeitungen 4.0 International.\n 
+			$this->lang->terms->de 	= "<b style=\"font-family:calibrib\">Nutzungsbedingungen:</b> Die e-Forschungsberichte {$this->metadata['year']}-{$this->metadata['volume']} des Deutschen Archäologischen Instituts stehen unter der Creative-Commons-Lizenz Namensnennung – Nicht kommerziell – Keine Bearbeitungen 4.0 International.\n 
 			Um eine Kopie dieser Lizenz zu sehen, besuchen Sie bitte http://creativecommons.org/licenses/by-nc-nd/4.0/";
-			$this->lang->terms->en  = "<b style=\"font-family:calibrib\">Terms of use:</b> ...";
+			$this->lang->terms->en  = "<b style=\"font-family:calibrib\">Terms of use:</b> The e-Research Reports {$this->metadata['year']}-{$this->metadata['volume']} of the Deutsches Archäologisches Institut is published under the Creative-Commons-Licence BY – NC – ND 4.0 International. <br> 
+			To see a copy of this licence visit http://creativecommons.org/licenses/by-nc-nd/4.0/";
 		}
 		
 	}
--- classes/pdfWorker/journalSpecific/ejb.class.php
+++ classes/pdfWorker/journalSpecific/ejb.class.php
@@ -2,9 +2,10 @@
 namespace dfm\pdfWorkers {
 	class ejb extends \dfm\pdfWorker {
 		function setMetadata($data) {
-			$this->lang->terms->de 	= "<b style=\"font-family:calibrib\">Nutzungsbedingungen:</b> Die e-Forschungsberichte {$this->metadata['year']}-{$this->metadata['volume']} des DAI stehen unter der Creative-Commons-Lizenz Namensnennung – Nicht kommerziell – Keine Bearbeitungen 4.0 International.\n 
+			$this->lang->terms->de 	= "<b style=\"font-family:calibrib\">Nutzungsbedingungen:</b> Der e-Jahresbericht {$this->metadata['year']} des Deutschen Archäologischen Instituts steht unter der Creative-Commons-Lizenz Namensnennung – Nicht kommerziell – Keine Bearbeitungen 4.0 International.<br> 
 			Um eine Kopie dieser Lizenz zu sehen, besuchen Sie bitte http://creativecommons.org/licenses/by-nc-nd/4.0/";
-			$this->lang->terms->en  = "<b style=\"font-family:calibrib\">Terms of use:</b> ...";
+			$this->lang->terms->en  = "<b style=\"font-family:calibrib\">Terms of use:</b> The e-Annual Report {$this->metadata['year']} of the Deutsches Archäologisches Institut is published under the Creative-Commons-Licence BY – NC – ND 4.0 International. <br> 
+			To see a copy of this licence visit http://creativecommons.org/licenses/by-nc-nd/4.0/";
 		}
 		
 	}
